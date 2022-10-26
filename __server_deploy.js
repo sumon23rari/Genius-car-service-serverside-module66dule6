7ce071f571429688aa820f1c56bc664d912d4acd
sumon23rari <sumon784969@gmail.com>
2022-10-26 22:57:21 +0600

    adding-steps

---
+++ __server_deploy.js
@@ -0,0 +1,15 @@
+/* 
+ -------------------------
+*one time for your computer
+--------------------------
+1. create heroku account
+2.  verify email address
+3. install heroku cli
+4. heroku login
+------------------------
+For each project on time
+------------------------
+1. heroku create
+2. make sure you: git add . git commit git push
+3. 
+*/
